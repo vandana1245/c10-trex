4f9921f6a26a7aeb4cbf07a7a774d9139110731e
vandana1245 <66421668+vandana1245@users.noreply.github.com>
2022-11-23 06:32:13 +0530

    Add files via upload

---
+++ Trex_Stage_0/sketch.js
@@ -0,0 +1,35 @@
+
+var trex;
+var trex_running;
+var edges;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+  trex = createSprite(50,160, 20.50);
+  trex.addAnimation("running", trex_running)
+
+  trex.scale = 0.5;
+
+  edges = createEdgeSprites();
+}
+
+function draw(){
+  background("white")
+
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+
+  trex.velocityY = trex.velocityY + 0.5
+
+  trex.collide(edges[3])
+  
+  drawSprites();
+}
